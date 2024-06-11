fce7d1bbc7c2036217e0ffd336024d7a0b18507e
Heemank Verma <heemankv@gmail.com>
2024-06-11 13:53:49 +0530

    update: added boilerplate code

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,9 @@
-use cosmwasm_std::{
-  entry_point, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
-};
-
-mod msg;
-mod contract;
-
-#[entry_point]
-pub fn instantiate(deps: DepsMut, env: Env, info: MessageInfo, msg: Empty)
--> StdResult<Response>
-{
-  contract::instantiate(deps, env, info, msg)
-}
-
-#[entry_point]
-pub fn query(deps: Deps, env: Env, msg: msg::QueryMsg)
--> StdResult<Binary>
-{
-  contract::query(deps, env, msg)
-}
+pub mod contract;
+mod error;
+pub mod liquidation;
+pub mod msg;
+pub mod state;
+pub mod query;
+pub mod external;
+#[cfg(test)]
+pub mod tests;
